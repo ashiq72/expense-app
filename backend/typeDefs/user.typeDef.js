039e10176ad199ef3652234f7953b97f9ef6e084
Md ashiquzzaman <ahmedashik18k@gmail.com>
2024-10-08 01:04:58 +0600

    Updated

---
+++ backend/typeDefs/user.typeDef.js
@@ -0,0 +1,14 @@
+const userTypeDef = `#graphql
+type User{
+    _id: ID!
+    username: String!
+    name: String!
+    password: String!
+    profilePicture: String
+    gender: String!
+}
+  type Query{
+    users
+  }
+
+`
