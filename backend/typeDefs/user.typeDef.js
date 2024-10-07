9e8d60d74f67fe32fadd1625a5ab54b07dc62c75
ashiq72 <ahmedashik18k@gmail.com>
2024-10-07 14:17:13 +0600

    Updated

--- backend/typeDefs/user.typeDef.js
+++ backend/typeDefs/user.typeDef.js
@@ -16,5 +16,20 @@ user(userId: ID): User
 type Mutation{
 signUp(input: SignUpInput!): User
 login(input: LoginInput!): User
+logout: LogoutResponse
+}
+input SignUpInput{
+    username: String!
+    name: String!
+    password: String!
+    gender: String!
+}
+input LoginInput{
+    username: String!
+    password: String!
+}
+type LogoutResponse{
+    message: String!
 }
 `
+export default userTypeDef;
